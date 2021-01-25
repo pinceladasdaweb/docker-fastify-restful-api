cf9257cb35c72360ad163763de96cb143555b3ea
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-01-25 13:02:25 -0300

    fix(db): close mongo connection after interrupt the server

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ server()
   .then(app => {
     app.listen(APP_PORT, '0.0.0.0')
       .then(_ => {
-        process.on('SIGINT', () => app.close())
-        process.on('SIGTERM', () => app.close())
+        process.on('SIGINT', () => {
+          app.mongo.connection.close()
+          app.close()
+          process.exit(0)
+        })
+          .on('SIGTERM', () => {
+            app.mongo.connection.close()
+            app.close()
+            process.exit(0)
+          })
       })
       .catch(err => {
         console.log('Error starting server: ', err)
