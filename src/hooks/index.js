3cf94ac031b5de6185102b31bf36939fd88d10a9
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-01-28 20:38:09 -0300

    feat(*): added fastify hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,15 @@
+const Sentry = require('@sentry/node')
+const { NODE_ENV } = require('../environment')
+const fastifyPlugin = require('fastify-plugin')
+
+module.exports = fastifyPlugin((fastify, options, next) => {
+  fastify.addHook('onError', (request, reply, error, done) => {
+    if (NODE_ENV !== 'development') {
+      Sentry.captureException(error)
+    }
+
+    done()
+  })
+
+  next()
+})
