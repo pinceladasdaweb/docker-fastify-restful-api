58790b2e138ba1572ef968cd326daaa72dce8871
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-02-05 23:26:49 -0300

    feat(routes): added route to auth user

--- src/routes/api/users/index.js
+++ src/routes/api/users/index.js
@@ -2,7 +2,8 @@ const { createSchema } = require('./schemas')
 const { userController } = require('../../../controllers')
 
 const userRoutes = async (app, options) => {
-  app.post('/', { schema: createSchema }, userController.register)
+  app.post('/auth', userController.auth)
+  app.post('/register', { schema: createSchema }, userController.register)
 }
 
 module.exports = userRoutes
