04a4506a44cd1a98072244ce7d5771b7de6632fd
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-02-05 23:27:30 -0300

    feat(user): added auth method

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,6 +1,26 @@
 const boom = require('boom')
+const bcrypt = require('bcryptjs')
 const { Users } = require('../models')
 const { UsersRepository } = require('../repositories')
+
+const auth = async (request, reply) => {
+  try {
+    const { email, password } = request.body
+
+    const user = new UsersRepository(Users)
+    const userInfo = await user.findOneBy('email', email)
+
+    if (userInfo && bcrypt.compareSync(password, userInfo.password)) {
+      const token = await reply.jwtSign({ id: userInfo._id, name: userInfo.name }, {
+        expiresIn: '7d'
+      })
+
+      reply.send({ token })
+    }
+  } catch (err) {
+    throw boom.boomify(err)
+  }
+}
 
 const register = async (request, reply) => {
   try {
@@ -16,5 +36,6 @@ const register = async (request, reply) => {
 }
 
 module.exports = {
+  auth,
   register
 }
