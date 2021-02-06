e279e516e765bf289ae6accc12f04ec60fc79ee0
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-02-06 14:47:25 -0300

    feat(models): added movies model

---
+++ src/models/movies.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
+const mongooseDelete = require('mongoose-delete')
+const mongooseTimestamp = require('mongoose-timestamp')
+const mongoosePaginate = require('mongoose-paginate-v2')
+const { genresEnum, languagesEnum, countryCodesEnum } = require('../enums')
+
+/**
+* Namespace: Movies
+*/
+const Schema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    index: true,
+    required: true
+  },
+  slug: {
+    type: String,
+    index: true,
+    slug: ['title', 'year']
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  releasedAt: {
+    type: Date
+  },
+  genres: [{
+    type: String,
+    enum: genresEnum,
+    index: true,
+    required: true
+  }],
+  director: {
+    type: String,
+    trim: true,
+    index: true,
+    required: true
+  },
+  writers: {
+    type: String,
+    trim: true,
+    index: true,
+    required: true
+  },
+  actors: {
+    type: String,
+    trim: true,
+    index: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  languages: [{
+    type: String,
+    enum: languagesEnum,
+    index: true,
+    required: true
+  }],
+  country: {
+    type: String,
+    enum: countryCodesEnum,
+    index: true,
+    required: true
+  },
+  imdbID: {
+    type: String,
+    trim: true,
+    index: true,
+    required: true
+  }
+}, {
+  usePushEach: true
+})
+
+Schema.plugin(slug)
+Schema.plugin(mongoosePaginate)
+Schema.plugin(mongooseTimestamp)
+Schema.plugin(mongooseDelete, {
+  deletedAt: true,
+  indexFields: true,
+  overrideMethods: true
+})
+
+module.exports = mongoose.model('Movies', Schema)
