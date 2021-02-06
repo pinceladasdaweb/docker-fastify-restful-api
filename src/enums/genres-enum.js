ec86fddb6fb400d3d4b545f4494a1ca56cb3e5c0
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-02-06 14:44:53 -0300

    feat(enums): added enums

---
+++ src/enums/genres-enum.js
@@ -0,0 +1,28 @@
+const genresEnum = [
+  'Action',
+  'Adventure',
+  'Animation',
+  'Biography',
+  'Comedy',
+  'Crime',
+  'Documentary',
+  'Drama',
+  'Family',
+  'Fantasy',
+  'Film Noir',
+  'History',
+  'Horror',
+  'Music',
+  'Musical',
+  'Mystery',
+  'Romance',
+  'Sci-Fi',
+  'Short',
+  'Sport',
+  'Superhero',
+  'Thriller',
+  'War',
+  'Western'
+]
+
+module.exports = genresEnum
---
+++ src/enums/index.js
@@ -0,0 +1,9 @@
+const genresEnum = require('./genres-enum')
+const languagesEnum = require('./languages-enum')
+const countryCodesEnum = require('./country-codes-enum')
+
+module.exports = {
+  genresEnum,
+  languagesEnum,
+  countryCodesEnum
+}
