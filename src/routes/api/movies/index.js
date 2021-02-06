f82fd2a4b9b812d594ad78f5f563cde820d9a4d2
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-02-06 20:50:38 -0300

    feat(movies): addd route to list all movies

--- src/routes/api/movies/index.js
+++ src/routes/api/movies/index.js
@@ -1,7 +1,8 @@
-const { createSchema } = require('./schemas')
+const { createSchema, listSchema } = require('./schemas')
 const { moviesController } = require('../../../controllers')
 
 const moviesRoutes = async (app, options) => {
+  app.get('/', { schema: listSchema }, moviesController.list)
   app.post('/', { schema: createSchema }, moviesController.create)
 }
 
