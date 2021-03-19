6e04d697a99d465d1e9bce638b1799d2f73cd399
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-03-19 18:26:28 -0300

    feat(*): use @hapi/boom instead of boom

--- src/controllers/movies-controller.js
+++ src/controllers/movies-controller.js
@@ -1,4 +1,4 @@
-const boom = require('boom')
+const boom = require('@hapi/boom')
 const { Movies } = require('../models')
 const { isValidObjectId } = require('mongoose')
 const { MoviesRepository } = require('../repositories')
