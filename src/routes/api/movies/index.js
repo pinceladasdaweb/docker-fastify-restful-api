a429acc21cc1dfe23d40f54abed349948cb1a4c6
Pedro Rogério <pinceladasdaweb@hotmail.com>
2021-05-09 15:00:57 -0300

    feat(*): added preValidation hook for auth routes

--- src/routes/api/movies/index.js
+++ src/routes/api/movies/index.js
@@ -2,11 +2,11 @@ const { moviesController } = require('../../../controllers')
 const { createSchema, listSchema, viewSchema, updateSchema } = require('./schemas')
 
 const moviesRoutes = async (app, options) => {
-  app.get('/', { schema: listSchema }, moviesController.list)
-  app.get('/:id', { schema: viewSchema }, moviesController.view)
-  app.post('/', { schema: createSchema }, moviesController.create)
-  app.patch('/:id', { schema: updateSchema }, moviesController.update)
-  app.delete('/:id', moviesController.exclude)
+  app.get('/', { schema: listSchema, preValidation: [app.authenticate] }, moviesController.list)
+  app.get('/:id', { schema: viewSchema, preValidation: [app.authenticate] }, moviesController.view)
+  app.post('/', { schema: createSchema, preValidation: [app.authenticate] }, moviesController.create)
+  app.patch('/:id', { schema: updateSchema, preValidation: [app.authenticate] }, moviesController.update)
+  app.delete('/:id', { preValidation: [app.authenticate] }, moviesController.exclude)
 }
 
 module.exports = moviesRoutes
